9794c7ab697d4bf523a99293978ee5344cdf52c1
hg222dv <hencee@gmail.com>
2016-06-06 02:01:01 +0000

    del. vendor/cache maybe that works

--- zafarya/vendor/cache/ruby/2.3.0/gems/jquery-timepicker-rails-1.4.3/lib/jquery-timepicker-rails/version.rb
+++
@@ -1,7 +0,0 @@
-module Jquery
-  module Timepicker
-    module Rails
-      VERSION = '1.4.3'
-    end
-  end
-end
--- zafarya/vendor/cache/ruby/2.3.0/gems/mailgun_rails-0.8.0/lib/mailgun_rails.rb
+++
@@ -1,8 +0,0 @@
-require 'action_mailer'
-require 'json'
-
-
-Dir[File.dirname(__FILE__) + '/mailgun/*.rb'].each {|file| require file }
-
-module Mailgun
-end
