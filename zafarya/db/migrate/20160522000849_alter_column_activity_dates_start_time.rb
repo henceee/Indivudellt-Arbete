39e40cd952756c6935d47b6b61120feb332994e2
hg222dv <hencee@gmail.com>
2016-06-06 02:24:29 +0000

    old migrations caused errors migrating, del.

--- zafarya/db/migrate/20160522000849_alter_column_activity_dates_start_time.rb
+++
@@ -1,6 +0,0 @@
-class AlterColumnActivityDatesStartTime < ActiveRecord::Migration
-   def change
-    remove_column :activity_dates, :start_date, :string
-    remove_column :activity_dates, :end_date, :string
-  end
-end
